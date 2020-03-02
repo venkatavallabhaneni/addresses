788ce56e6d4a670d0163738dfe55131ec19e4dfc
venkat vallabhaneni <venkata.vallabhaneni@gmail.com>
2020-03-02 23:25:45 +0530

    simple address api

--- src/main/java/com/venkat/vallabhaneni/Addresses/AddressesController.java
+++ src/main/java/com/venkat/vallabhaneni/Addresses/AddressesController.java
@@ -1,4 +1,18 @@
 package com.venkat.vallabhaneni.Addresses;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
 public class AddressesController {
+
+  @GetMapping("/address/{name}")
+  public ResponseEntity<String> printAddress(@PathVariable String name) {
+    return new ResponseEntity<>("Venkata Rao Vallabhaneni, Bangalore", HttpStatus.OK);
+  }
 }
